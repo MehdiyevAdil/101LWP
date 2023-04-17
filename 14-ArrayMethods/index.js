c7eb64ba3c8b5ddadd84fd5a1fa94b8c580c0346
MehdiyevAdil <126787138+MehdiyevAdil@users.noreply.github.com>
2023-04-18 00:31:30 +0300

    Add files via upload

---
+++ 14-ArrayMethods/index.js
@@ -0,0 +1,29 @@
+
+// 1) Arrayin bütün elementlərini 3 xarakterə endirən funksiya yazın
+// let arr = ["lankaran","baki","masalli","lerik","astara"];
+// function substrElement(arr){
+// let element=arr.map(item => item.substr(0,3));
+// return element;
+// }
+// console.log(substrElement(arr));
+
+
+// 2) Kiçik L hərfi ilə başlayan elementləri silən funksiya yazın
+// let arr = ["lankaran","baki","masalli","lerik","astara"];
+// function removeElement(arr) {
+//     return arr.filter(function (element) {
+//       return element.charAt(0) !== "l";
+//     });
+//   }
+//   console.log(removeElement(arr))
+
+
+// 3)Verilmiş arrayin 2-ci elementinə [mandalin,limon] arrayini push 
+// eden funksiya yazin. Verilmiş arrayin elementini saxlamaq şərti ilə.
+// let arr = ["lankaran","baki","masalli","lerik","astara"];
+// function spliceElement(arr) {
+//     arr.splice(2, 0, ["Mandalin", "Limon"]);
+//     return arr;
+// }
+// console.log(spliceElement(arr))
+
