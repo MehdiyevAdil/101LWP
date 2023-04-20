19af449967659c1648867019cdd9e1fe06eee8e7
MehdiyevAdil <126787138+MehdiyevAdil@users.noreply.github.com>
2023-04-20 06:14:39 +0300

    Add files via upload

---
+++ 16-DomCardExample/assests/js/index.js
@@ -0,0 +1,34 @@
+
+const s7 = document.getElementById("s7");
+
+s7.addEventListener("click", function () {
+        this.style.backgroundColor = "mediumspringgreen";
+        
+})
+
+const s8 = document.getElementById("s8");
+
+s8.addEventListener("click", function () {
+        this.style.backgroundColor = "mediumspringgreen";
+
+})
+const s9 = document.getElementById("s9");
+
+s9.addEventListener("click", function () {
+        this.style.backgroundColor = "mediumspringgreen";
+        
+})
+
+const s10 = document.getElementById("s10");
+
+s10.addEventListener("click", function () {
+        this.style.backgroundColor = "mediumspringgreen";
+        
+})
+
+const s11 = document.getElementById("s11");
+
+s11.addEventListener("click", function () {
+        this.style.backgroundColor = "mediumspringgreen";
+        
+})
